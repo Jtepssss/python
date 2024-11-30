c776c84e10ab2b79b81cb6d962683cfeeee3bff5
tu- <tu->
2024-11-30 11:12:02 -0500

    a

---
+++ operadores.py
@@ -0,0 +1,16 @@
+#pedimos datos
+
+N1 = int(input("Ingrese un numero: "))
+N2 = int(input("Ingrese otro numero: "))
+
+#operadores de comparacion
+igual =( N1 == N2)
+Mayor =( N1 > N2)
+Menor =( N1 < N2)
+diferente =( N1 != N2)
+
+#imprimimos resultados
+print(igual)
+print(Mayor)
+print(Menor)
+print(diferente)
